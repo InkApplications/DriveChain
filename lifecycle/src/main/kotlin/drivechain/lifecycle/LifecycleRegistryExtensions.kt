764e36d5025b742d119e49129b75001cfe16201d
Renee Vandervelde <Renee@ReneeVandervelde.com>
2017-10-22 12:30:14 -0500

    Change `addObservers` extension to use Lifecycle instead of LifecycleRegistry
    LifecycleRegistry isn't being used anymore in the support library. So this has gotta
    move.

--- lifecycle/src/main/kotlin/drivechain/lifecycle/LifecycleRegistryExtensions.kt
+++ lifecycle/src/main/kotlin/drivechain/lifecycle/LifecycleRegistryExtensions.kt
@@ -1,7 +1,7 @@
 package drivechain.lifecycle
 
+import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.LifecycleObserver
-import android.arch.lifecycle.LifecycleRegistry
 
 /** Add More than one observer at a time. What a novel concept. */
-fun LifecycleRegistry.addObservers(observers: Collection<LifecycleObserver>) = observers.forEach { addObserver(it) }
+fun Lifecycle.addObservers(observers: Collection<LifecycleObserver>) = observers.forEach { addObserver(it) }
