79e94f5f1ca1de5734625e7622b46761a73cf6ad
Renee Vandervelde <Renee@ReneeVandervelde.com>
2017-11-04 09:53:39 -0500

    Allow nullable formatting arguments.

--- logger/src/main/kotlin/drivechain/logger/EmptyLogger.kt
+++ logger/src/main/kotlin/drivechain/logger/EmptyLogger.kt
@@ -5,14 +5,14 @@ package drivechain.logger
  * log filtering.
  */
 class EmptyLogger: Logger {
-    override fun debug(message: String, vararg args: Any) = Unit
-    override fun debug(cause: Throwable, message: String, vararg args: Any) = Unit
-    override fun error(message: String, vararg args: Any) = Unit
-    override fun error(cause: Throwable, message: String, vararg args: Any) = Unit
-    override fun info(message: String, vararg args: Any) = Unit
-    override fun info(cause: Throwable, message: String, vararg args: Any) = Unit
-    override fun trace(message: String, vararg args: Any) = Unit
-    override fun trace(cause: Throwable, message: String, vararg args: Any) = Unit
-    override fun warn(message: String, vararg args: Any) = Unit
-    override fun warn(cause: Throwable, message: String, vararg args: Any) = Unit
+    override fun debug(message: String, vararg args: Any?) = Unit
+    override fun debug(cause: Throwable, message: String, vararg args: Any?) = Unit
+    override fun error(message: String, vararg args: Any?) = Unit
+    override fun error(cause: Throwable, message: String, vararg args: Any?) = Unit
+    override fun info(message: String, vararg args: Any?) = Unit
+    override fun info(cause: Throwable, message: String, vararg args: Any?) = Unit
+    override fun trace(message: String, vararg args: Any?) = Unit
+    override fun trace(cause: Throwable, message: String, vararg args: Any?) = Unit
+    override fun warn(message: String, vararg args: Any?) = Unit
+    override fun warn(cause: Throwable, message: String, vararg args: Any?) = Unit
 }
