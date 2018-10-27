33f8266f992b7a1c23b83610c8fcd4b628fb2c1d
Renee Vandervelde <Renee@ReneeVandervelde.com>
2018-10-27 09:31:19 -0500

    Add Bootstrap module for less boilerplate.
    The bootstrap module doesn't do anything new, but instead reduces
    the amount of code needed for setup, particularly on new projects.

---
+++ bootstrap/src/main/kotlin/drivechain/bootstrap/BootstrapModule.kt
@@ -0,0 +1,20 @@
+package drivechain.bootstrap
+
+import dagger.Module
+import drivechain.androidservices.AndroidApplicationModule
+import drivechain.androidservices.AndroidSystemServiceModule
+import drivechain.lifecycle.LifecycleModule
+import drivechain.logger.AutoLoggers
+
+/**
+ * A Single module for base projects to include for typical defaults.
+ *
+ * Using this module is not required, and is simply a shortcut to reduce
+ * boilerplate in typical setups.
+ */
+@Module(includes = [
+    AutoLoggers::class,
+    LifecycleModule::class,
+    AndroidApplicationModule::class,
+    AndroidSystemServiceModule::class
+]) class BootstrapModule
