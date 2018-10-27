3df729c2116226eb5bc83cda8084b33e998bd0f0
Renee Vandervelde <Renee@ReneeVandervelde.com>
2018-10-27 11:09:21 -0500

    Move activity lifecycle binding into appKernel

--- lifecycle/src/main/kotlin/drivechain/lifecycle/AppKernel.kt
+++ lifecycle/src/main/kotlin/drivechain/lifecycle/AppKernel.kt
@@ -1,6 +1,9 @@
 package drivechain.lifecycle
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import drivechain.lifecycle.application.*
@@ -26,6 +29,19 @@ class AppKernel(
         it.onCreate(application)
     } action {
         lifecycleSubscribers.forEach { it.onCreate(application) }
+
+        application.registerActivityLifecycleCallbacks(object: Application.ActivityLifecycleCallbacks {
+            override fun onActivityPaused(activity: Activity) = Unit
+            override fun onActivityResumed(activity: Activity) = Unit
+            override fun onActivityStarted(activity: Activity) = Unit
+            override fun onActivityDestroyed(activity: Activity) = Unit
+            override fun onActivitySaveInstanceState(activity: Activity, p1: Bundle?) = Unit
+            override fun onActivityStopped(activity: Activity) = Unit
+            override fun onActivityCreated(activity: Activity, p1: Bundle?) {
+                activity as? AppCompatActivity ?: return
+                bindLifecycle(activity.lifecycle)
+            }
+        })
     }
 
     /**
--- sample-project/src/main/java/com/acme/example/BaseActivity.kt
+++ sample-project/src/main/java/com/acme/example/BaseActivity.kt
@@ -5,26 +5,18 @@ import androidx.annotation.CallSuper
 import androidx.appcompat.app.AppCompatActivity
 import com.acme.example.dependencyinjection.ActivityComponent
 import drivechain.androidservices.AndroidCompatActivityModule
-import drivechain.lifecycle.AppKernel
-import javax.inject.Inject
 
 abstract class BaseActivity: AppCompatActivity() {
     val exampleApplication get() = application as ExampleApplication
-
     val component by lazy {
-        exampleApplication.component
-            .createActivityComponent(AndroidCompatActivityModule(this))
+        exampleApplication.component.createActivityComponent(AndroidCompatActivityModule(this))
     }
 
-    @Inject lateinit var appKernel: AppKernel
-
-    @CallSuper
-    override fun onCreate(savedInstanceState: Bundle?) {
+    @CallSuper override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         component.injectBase(this)
         injectSelf(component)
-        appKernel.bindLifecycle(lifecycle)
     }
 
     abstract fun injectSelf(component: ActivityComponent)
