f9fde6ab0b7bc07c3f236519cf1c29403616c59d
Renee Vandervelde <Renee@ReneeVandervelde.com>
2019-01-27 18:44:08 -0600

    Bind Sample activity to lifecycle

--- sample-project/src/main/java/com/acme/example/BaseActivity.kt
+++ sample-project/src/main/java/com/acme/example/BaseActivity.kt
@@ -5,6 +5,8 @@ import android.support.annotation.CallSuper
 import android.support.v7.app.AppCompatActivity
 import com.acme.example.dependencyinjection.ActivityComponent
 import drivechain.androidservices.AndroidCompatActivityModule
+import drivechain.lifecycle.AppKernel
+import javax.inject.Inject
 
 abstract class BaseActivity: AppCompatActivity() {
     val exampleApplication get() = application as ExampleApplication
@@ -12,12 +14,15 @@ abstract class BaseActivity: AppCompatActivity() {
         exampleApplication.component.createActivityComponent(AndroidCompatActivityModule(this))
     }
 
+    @Inject lateinit var kernel: AppKernel
+
     @CallSuper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         component.injectBase(this)
         injectSelf(component)
+        kernel.bindLifecycle(lifecycle)
     }
 
     abstract fun injectSelf(component: ActivityComponent)
