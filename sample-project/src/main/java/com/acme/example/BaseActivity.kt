2595ed95ebc137e776a9bb8a09b0c6b87cb65c19
Renee Vandervelde <Renee@ReneeVandervelde.com>
2019-01-27 19:15:19 -0600

    Add Example lifecycle observer

--- sample-project/src/main/java/com/acme/example/BaseActivity.kt
+++ sample-project/src/main/java/com/acme/example/BaseActivity.kt
@@ -5,8 +5,6 @@ import android.support.annotation.CallSuper
 import android.support.v7.app.AppCompatActivity
 import com.acme.example.dependencyinjection.ActivityComponent
 import drivechain.androidservices.AndroidCompatActivityModule
-import drivechain.lifecycle.AppKernel
-import javax.inject.Inject
 
 abstract class BaseActivity: AppCompatActivity() {
     val exampleApplication get() = application as ExampleApplication
@@ -14,15 +12,12 @@ abstract class BaseActivity: AppCompatActivity() {
         exampleApplication.component.createActivityComponent(AndroidCompatActivityModule(this))
     }
 
-    @Inject lateinit var kernel: AppKernel
-
     @CallSuper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         component.injectBase(this)
         injectSelf(component)
-        kernel.bindLifecycle(lifecycle)
     }
 
     abstract fun injectSelf(component: ActivityComponent)
--- sample-project/src/main/java/com/acme/example/dependencyinjection/ApplicationComponent.kt
+++ sample-project/src/main/java/com/acme/example/dependencyinjection/ApplicationComponent.kt
@@ -1,13 +1,15 @@
 package com.acme.example.dependencyinjection
 
 import com.acme.example.ExampleApplication
+import com.acme.example.examples.ExamplesModule
 import dagger.Component
 import drivechain.androidservices.AndroidCompatActivityModule
 import drivechain.bootstrap.BootstrapModule
 
 @Component(modules = [
     BootstrapModule::class,
-    AppConfigModule::class
+    AppConfigModule::class,
+    ExamplesModule::class
 ])
 interface ApplicationComponent {
     fun inject(target: ExampleApplication)
